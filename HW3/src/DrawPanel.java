025226efb2ec35520eaf55be1e37de91ed5868e6
p5351280 <p5351280@gmail.com>
2018-05-19 17:09:44 +0800

    base finish

---
+++ HW3/src/DrawPanel.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class DrawPanel extends JPanel{
+
+	
+	public DrawPanel() {
+		setBackground(Color.WHITE);
+		
+		
+	}
+}
---
+++ HW3/src/MyOval.java
@@ -0,0 +1,44 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class MyOval {
+	private int x1;
+	private int y1;
+	private int x2;
+	private int y2;
+	private Color color;
+	private boolean filled;
+	
+	public MyOval(int x1, int y1, int x2, int y2, Color color, boolean filled) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.color = color;
+		this.filled = filled;
+	}
+	
+	public int getUpperLeftX() {
+		return Math.min(x1, x2);
+	}
+	
+	public int getUpperLeftY() {
+		return Math.min(y1, y2);
+	}
+	
+	public int getWidth() {
+		return Math.abs(x1-x2);
+	}
+	
+	public int getHeight() {
+		return Math.abs(y1-y2);
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(color);
+		if(filled)
+			g.fillOval(getUpperLeftX(), getUpperLeftX(), getWidth(), getHeight());
+		else
+			g.drawOval(getUpperLeftX(), getUpperLeftY(), getWidth(), getHeight());
+	}
+}
